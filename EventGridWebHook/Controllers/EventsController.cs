28b724ecf83cfcf79b0cabeaf6b6b5b0c50f9606
Vivek Sharma <vivek.se@gmail.com>
2018-08-08 00:22:51 -0400

    initial commit

---
+++ EventGridWebHook/Controllers/EventsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.EventGrid.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace EventGridWebHook.Controllers
+{
+    [Route("api/[controller]")]
+    public class EventsController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody]List<GridEvent<object>> events)
+        {
+            if (events == null)
+            {
+                return Ok();
+            }
+            foreach (var event1 in events)
+            {
+                if (event1.EventType == "Microsoft.EventGrid.SubscriptionValidationEvent")
+                {
+                    var eventData = JsonConvert.DeserializeObject<SubscriptionValidationEventData>(event1.Data.ToString());
+                    var responseData = new SubscriptionValidationResponse();
+                    responseData.ValidationResponse = eventData.ValidationCode;
+                    return Ok(responseData);
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ EventGridWebHook/GridEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EventGridWebHook
+{
+    public class GridEvent<T> where T : class
+    {
+        public string Id { get; set; }
+        public string Subject { get; set; }
+        public string EventType { get; set; }
+        public T Data { get; set; }
+        public DateTime EventTime { get; set; }
+
+        public GridEvent()
+        {
+            Id = Guid.NewGuid().ToString();
+            EventTime = DateTime.UtcNow;
+        }
+    }
+}
